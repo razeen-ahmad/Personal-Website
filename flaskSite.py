cbd5cc43cabba673b7c3b11a9a2ab2c55c42bea9
razeen-ahmad <rahmad875@gmail.com>
2020-08-31 17:48:31 -0500

    added website template file, other pages children with blocks

--- flaskSite.py
+++ flaskSite.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', page_name = "Home")
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', page_name = "About")
 
 @app.route("/portfolio")
 def portfolio():
-    return render_template('portfolio.html')
+    return render_template('portfolio.html', page_name = "Portfolio")
 
 if(__name__)=='__main__':
     app.run(debug = True)
